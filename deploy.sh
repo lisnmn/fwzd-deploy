d04b22fa0827be86e5465c67f92d4288f39e7167
lisnmn <39209084+lisnmn@users.noreply.github.com>
2020-04-21 23:17:49 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # cd 到构建输出的目录下 
-cd vue-fwzd
+# cd vue-fwzd
 
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
@@ -22,4 +22,4 @@ git push -f git@github.com:lisnmn/lisnmn.github.io.git master
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# cd -
